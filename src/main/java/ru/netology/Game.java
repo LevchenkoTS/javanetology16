0c715aea2963917872fdafd4b1025431a1845a9b
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2022-12-14 22:33:43 +0300

    hw 16.2 corrected

--- src/main/java/ru/netology/Game.java
+++ src/main/java/ru/netology/Game.java
@@ -1,31 +1,31 @@
 package ru.netology;
 
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Game {
 
 
-    HashMap<Integer, Player> players = new HashMap<>();
+    private HashMap <String, Integer> players = new HashMap<>();
 
 
     public void register(Player player) {
-        players.put(player.getId(), player);
+        players.put(player.getName(), player.getStrength());
     }
 
-    public Player findByName(String name) {
-        for (Player player : players.values()) {
-            if (player.getName().equalsIgnoreCase(name)) {
-                return player;
+    public Integer findByName(String name) {
+        for (String key : players.keySet()) {
+            if (key.equals(name)) {
+                Integer value = players.get(key);
+                return value;
             }
         }
         return null;
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = findByName(playerName1);
-        Player player2 = findByName(playerName2);
+        Integer player1 = findByName(playerName1);
+        Integer player2 = findByName(playerName2);
 
         if (player1 == null) {
             throw new NotRegisteredException(playerName1);
@@ -34,10 +34,10 @@ public class Game {
             throw new NotRegisteredException(playerName2);
         }
 
-        if (player1.getStrength() > player2.getStrength()) {
+        if (player1 > player2) {
             return 1;
         }
-        if (player1.getStrength() < player2.getStrength()) {
+        if (player1 < player2) {
             return 2;
         }
         return 0;
